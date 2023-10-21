76ac115ee4054eeab148971c928ab4b79c092a99
jhdjames37 <jhdjames37@outlook.com>
2023-10-21 11:58:28 +0800

    Add simple lsp display test.

---
+++ lsp-test/cpp/1.cpp
@@ -0,0 +1,15 @@
+#include <cstdio>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+
+
+void foo() {
+}
+int main() {
+  int a = 3;
+  std::vector<int> vec {1, 2, 3};
+  auto it = vec.begin();
+  foo();
+  printf("%d\n", 233);
+}
