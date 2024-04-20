9cc63a013f1ec24368c175492badebf459f10976
jhdjames37 <jhdjames37@outlook.com>
2024-04-20 14:12:29 +0800

    Update multiple-cursors

--- setup.el
+++ setup.el
@@ -62,7 +62,11 @@
 
 ;; multiple cursors
 (use-package multiple-cursors
-  :bind ("C-S-c C-S-c" . mc/edit-lines)
+  :bind
+  ("C-S-c C-S-c" . mc/edit-lines)
+  ("C->" . mc/mark-next-like-this)
+  ("C-<" . mc/mark-previous-like-this)
+  ("C-c C-<" . mc/mark-all-like-this)
   )
 
 ;; hs-minor-mode
