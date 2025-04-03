dc5bb664719597c8ed58b279957897e3acf8405c
jhdjames37 <jhdjames37@outlook.com>
2025-04-03 17:14:38 +0800

    Update settings config.

---
+++ settings.example.el
@@ -0,0 +1,18 @@
+;;; settings.el --- Summary
+;;; Commentary:
+
+;;; Here contains all machine dependent parameters,
+
+;;; Code:
+
+(setq my/use-zsh t
+      my/use-builtin-theme nil
+      my/enabled-lang
+      '(cpp python js rust latex org markdown typst yaml dotenv dockerfile caddyfile bison)
+      my/minibuffer-backend 'vertico
+      my/font-scale 1.8
+      my/enabled-feat '(ellama)
+      my/use-lsp-frontend 'eglot
+
+(provide 'settings)
+;;; settings.el ends here
