48b7b4144699056d411d510af4dbee4fd5803dd4
jhdjames37 <jhdjames37@outlook.com>
2025-08-02 16:22:58 +0800

    Small checkout.

--- init.el
+++ init.el
@@ -11,6 +11,7 @@
 ;;    (load-file (expand-file-name "config.el" user-emacs-directory))
 ;;  (org-babel-load-file (expand-file-name "config.org" user-emacs-directory)))
 
+;;(load-file (expand-file-name "config.el" user-emacs-directory))
 (org-babel-load-file (expand-file-name "config.org" user-emacs-directory))
 
 ;;; setup load path
