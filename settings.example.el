70af46096a2a21b74d2d3072e83a1d49851e7202
jhdjames37 <jhdjames37@outlook.com>
2025-12-24 19:35:32 +0800

    Updated support for custom configs.

--- settings.example.el
+++ settings.example.el
@@ -7,11 +7,12 @@
 
 (setq my/use-zsh t
       my/use-builtin-theme nil
+      my/doom-theme 'doom-fairy-floss
       my/enabled-lang
       '(cpp python js rust latex org markdown typst yaml dotenv dockerfile caddyfile bison)
       my/minibuffer-backend 'vertico
       my/font-scale 1.8
-      my/enabled-feat '(ellama)
+      my/enabled-feat '(ellama aidermacs pdf workspace)
       my/use-lsp-frontend 'eglot)
 
 (provide 'settings)
