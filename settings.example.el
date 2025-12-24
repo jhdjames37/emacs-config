02b644aa34d0eb1adc46a34d4f1f3366f3006acd
jhdjames37 <jhdjames37@outlook.com>
2025-12-24 19:42:44 +0800

    Fix (lang support)

--- settings.example.el
+++ settings.example.el
@@ -9,7 +9,7 @@
       my/use-builtin-theme nil
       my/doom-theme 'doom-fairy-floss
       my/enabled-lang
-      '(cpp python js rust latex org markdown typst yaml dotenv dockerfile caddyfile bison)
+      '(cpp python python-jupyter js rust latex org markdown typst yaml dotenv dockerfile caddyfile bison)
       my/minibuffer-backend 'vertico
       my/font-scale 1.8
       my/enabled-feat '(ellama aidermacs pdf workspace)
